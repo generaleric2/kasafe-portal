fe77e163e56934e2ff35a475457c1730634c9549
eric <maneeric20@gmail.com>
2024-09-20 11:45:46 +0300

    temp

--- kasafe admin/src/Admin/Pages/register.jsx
+++ kasafe admin/src/Admin/Pages/register.jsx
@@ -1,73 +1,77 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { MDBInput, MDBBtn } from 'mdb-react-ui-kit';
 import MiniDrawer from "../Components/sidebar";
 import './inbox.css';
 
 const Register = () => {
+
+  useEffect(() => {
+    // Check if dark mode is active and apply appropriate styles
+    const isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+    if (isDarkMode) {
+      document.body.classList.add('dark-mode');
+    }
+  }, []);
+
   return (
     <div>
       <MiniDrawer />
       <div className="col-md-10">
-        <div className="container-fluid form-style" style={{ marginLeft: '330px', marginTop: "2px" }}>
+        <div className="container-fluid form-style" style={{ marginLeft: '330px', marginTop: "-20px", width: "100%" }}>
           <form className="form" id="registerFO" action="#" name="register" method="POST" onSubmit={() => {}}>
-            <h1 className="text-muted text-center">Register an Admin</h1>
+            <h1 className="text-muted text-center" style={{marginTop: "-10px"}}>Register an Admin</h1>
             <hr />
-            {/* Add your form fields here */}
             <div className="row">
-              {/* Example form field */}
+              {/* Form Fields */}
               <div className="col" style={{ display: 'flex' }}>
-                <div className="form-field" style={{ marginRight: '10px', flex: '0 0 50%' }}>
+                <div className="form-field" style={{ marginRight: '10px', flex: '0 0 100%' }}>
                   <label htmlFor="firstName">First Name:<small className="asterisk"> *</small></label>
-                  <input id="firstName" type="text" name="firstName" />
-                  <small id="firstnameerr" style={{ color: 'red', fontWeight: 'bold', fontSize: 'small' }}></small>
+                  <input id="firstName" type="text" name="firstName" className="form-input" />
+                  <small id="firstnameerr" className="error-text"></small>
                 </div>
-                <div className="form-field" style={{ flex: '0 0 50%' }}>
+                <div className="form-field" style={{ flex: '0 0 100%' }}>
                   <label htmlFor="lastName">Last Name:<small className="asterisk"> *</small></label>
-                  <input id="lastName" type="text" name="lastName" />
-                  <small id="lastnameerr" style={{ color: 'red', fontWeight: 'bold', fontSize: 'small' }}></small>
+                  <input id="lastName" type="text" name="lastName" className="form-input" />
+                  <small id="lastnameerr" className="error-text"></small>
                 </div>
               </div>
+
               <div className="col" style={{ display: 'flex' }}>
-                <div className="form-field" style={{ marginRight: '10px', flex: '0 0 50%' }}>
+                <div className="form-field" style={{ marginRight: '10px', flex: '0 0 100%' }}>
                   <label htmlFor="address">Address:<small className="asterisk"> *</small></label>
-                  <input id="address" type="text" name="address" />
-                  <small id="addresserr" style={{ color: 'red', fontWeight: 'bold', fontSize: 'small' }}></small>
+                  <input id="address" type="text" name="address" className="form-input" />
+                  <small id="addresserr" className="error-text"></small>
                 </div>
-                <div className="form-field" style={{ flex: '0 0 50%' }}>
-                  <label htmlFor="phonenumber">Phonenumber:<small className="asterisk"> *</small></label>
-                  <input id="phonenumber" type="text" name="phonenumber" />
-                  <small id="phonenumbererr" style={{ color: 'red', fontWeight: 'bold', fontSize: 'small' }}></small>
+                <div className="form-field" style={{ flex: '0 0 100%' }}>
+                  <label htmlFor="phonenumber">Phone number:<small className="asterisk"> *</small></label>
+                  <input id="phonenumber" type="text" name="phonenumber" className="form-input" />
+                  <small id="phonenumbererr" className="error-text"></small>
                 </div>
               </div>
+
               <div className="col" style={{ display: 'flex' }}>
-                <div className="form-field" style={{ marginRight: '10px', flex: '0 0 50%' }}>
+                <div className="form-field" style={{ marginRight: '10px', flex: '0 0 100%' }}>
                   <label htmlFor="occupation">Occupation:<small className="asterisk"> *</small></label>
-                  <input id="occupation" type="text" name="occupation" />
-                  <small id="occupationerr" style={{ color: 'red', fontWeight: 'bold', fontSize: 'small' }}></small>
+                  <input id="occupation" type="text" name="occupation" className="form-input" />
+                  <small id="occupationerr" className="error-text"></small>
                 </div>
-                <div className="form-field" style={{ flex: '0 0 50%' }}>
+                <div className="form-field" style={{ flex: '0 0 100%' }}>
                   <label htmlFor="email">Email:<small className="asterisk"> *</small></label>
-                  <input id="email" type="text" name="email" />
-                  <small id="emailerr" style={{ color: 'red', fontWeight: 'bold', fontSize: 'small' }}></small>
+                  <input id="email" type="text" name="email" className="form-input" />
+                  <small id="emailerr" className="error-text"></small>
                 </div>
               </div>
+
               <div className="form-field">
                 <label htmlFor="password">Password:<small className="asterisk"> *</small></label>
-                <input id="password" type="text" name="password" />
-                <small id="passworderr" style={{ color: 'red', fontWeight: 'bold', fontSize: 'small' }}></small>
+                <input id="password" type="password" name="password" className="form-input" />
+                <small id="passworderr" className="error-text"></small>
               </div>
-              {/* Add more form fields */}
             </div>
-            {/* Add more sections of the form */}
             <hr className="mt-1" />
-            <div className="row">
-              {/* Add more form fields */}
-            </div>
-            <div className="row">
-            </div>
             <div className="container FO-form-btn">
               <div className="button1">
-                <button className="btn btn-primary mt-1 form-add-btn mr-auto" type="submit" style={{color: "white", backgroundColor: "black"}}>Add New</button>
+                <button className="btn btn-primary mt-1 form-add-btn mr-auto" type="submit" style={{color: "white", backgroundColor: "black", marginLeft: "700px"}}>Register</button>
               </div>
             </div>
           </form>
--- kasafe admin/src/App.jsx
+++ kasafe admin/src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Admin/Pages/home"
-import InboxPage from "./Admin/Pages/inbox"
-import Mail from "./Admin/Pages/mail"
-import Register from "./Admin/Pages/register"
-import Users from "./Admin/Pages/users"
-import Transactions from "./Admin/Pages/transactions"
-import Groups from "./Admin/Pages/groups"
-import Login from "./Admin/Pages/login"
-import Twofactor from "./Admin/Pages/2fa"
+// import InboxPage from "./Admin/Pages/inbox"
+// import Mail from "./Admin/Pages/mail"
+// import Register from "./Admin/Pages/register"
+// import Users from "./Admin/Pages/users"
+// import Transactions from "./Admin/Pages/transactions"
+// import Groups from "./Admin/Pages/groups"
+// import Login from "./Admin/Pages/login"
+// import Twofactor from "./Admin/Pages/2fa"
 
 function App() {
   return (
@@ -15,14 +15,14 @@ function App() {
 <Router>
   <Routes>
   <Route path="/home" element={<Home/>}/>
-  <Route path="/inbox" element={<InboxPage/>}/>
+  {/* <Route path="/inbox" element={<InboxPage/>}/>
   <Route path="/mail" element={<Mail/>}/>
   <Route path="/register" element={<Register/>}/>
   <Route path="/users" element={<Users/>}/>
   <Route path="/transactions" element={<Transactions/>}/>
   <Route path="/groups" element={<Groups/>}/>
   <Route path="/" element={<Login/>}/>
-  <Route path="/2fa" element={<Twofactor/>}/>
+  <Route path="/2fa" element={<Twofactor/>}/> */}
   </Routes>
 </Router>
 </div>
